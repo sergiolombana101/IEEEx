02dcf5be67ac2805a988a0d0757ac58fddc7770e
NadiaMok <mokhirevanadezda@gmail.com>
2019-10-19 00:35:35 -0400

    :(

--- contest.java
+++ contest.java
@@ -11,30 +11,38 @@ class Main {
 	}
 
 	public static void permutation(){
-		String [] len = in.nextLine().split(" ");
 		Scanner in = new Scanner(System.in);
+		String [] len = in.nextLine().split(" ");
 		String[] arr = in.nextLine().split(" ");
 		String[] set = in.nextLine().split(" ");
 
 		int totalLen = arr.length + set.length;
 		Integer [] result = new Integer[totalLen];
+		int ind = 0;
 
-		if(arr.length != len[0] || set.length != len[1]){
+		if(arr.length != Integer.parseInt(len[0]) || set.length != Integer.parseInt(len[1])){
 			System.out.println(totalLen);
         	in.close();
         	System.exit(0);
 		}
-		//loop to ckeck if current num is small than next - insert
-
-		for(int i = 0; i < set.length; i++){
-			for(int j = 0; j < arr.length; j++){
+		for(int j = 0; j < arr.length; j++){
+			for(int i = set.length-1; i >0; i--){
 				if(Integer.parseInt(set[i]) < Integer.parseInt(arr[j])){
-						
+					result[ind] = Integer.parseInt(set[i]);
+				
+				}else{
+					result[ind] = Integer.parseInt(arr[i]);
 				}
+				ind++;
 			}
+			//ind++;
+			
 		}
+		
 
-        System.out.println(totalLen);
+		for (int i = 0; i < result.length; i++){
+        	System.out.println(result[i]);
+		}
 
         in.close();
         System.exit(0);
